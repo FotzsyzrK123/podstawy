397d5ea1d764c3b5a62d616e90ac7d05b06b9249
FotzsyzrK123 <k.koska94@gmail.com>
2019-06-09 23:05:29 +0200

    forowanie

---
+++ Forowanie.java
@@ -0,0 +1,12 @@
+public class Forowanie{
+  public static void main(String[] args){
+	  int la;
+	  la = Integer.parseInt(args[0]);
+	  for(int i=0; i<la; i++){
+		  System.out.println("la");
+	  }
+	  System.out.println("Koniec pętli");
+	  
+	  
+  }
+}
