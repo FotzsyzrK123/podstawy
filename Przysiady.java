3866e429d3ac72aeb4608e5e47b1d6efa9e7238c
FotzsyzrK123 <k.koska94@gmail.com>
2019-06-10 08:35:02 +0200

    przysiadywanie

--- Przysiady.java
+++ Przysiady.java
@@ -3,16 +3,22 @@ public static void main(String[] args){
 	int przysiady;
 	przysiady = Integer.parseInt(args[0]);
 	if((przysiady%2)==0){
-		System.out.println("Gratulacje! Zrobiles " + przysiady/2 + " przysiadow");
+		if((przysiady/2)<2)
+		System.out.println("Gratulacje! Zrobiles " + przysiady/2 + " przysiad!");
+		else if((przysiady/2)<=4)
+		System.out.println("Gratulacje! Zrobiles " + przysiady/2 + " przysiady!");	
+		else
+		System.out.println("Gratulacje! Zrobiles " + przysiady/2 + " przysiadow!");
 	}		
 	else{
 		if((przysiady/2)<2)
-			System.out.println("Nie len sie! Jestes na dole. Rusz dupe do pelnego przysiadu!" + " Zrobiles dopiero " + przysiady/2 + " przysiad!");
-		else if((przysiady/2)<4)
-			System.out.println("Nie len sie! Jestes na dole. Rusz dupe do pelnego przysiadu!" + " Zrobiles dopiero " + przysiady/2 + " przysiady!");
+			System.out.println("Nie len sie! Jestes na dole. Rusz dupe do pelnego przysiadu!" + " Zrobiles dopiero " + przysiady/2 + " pelny przysiad!");
+		else if((przysiady/2)<=4)
+			System.out.println("Nie len sie! Jestes na dole. Rusz dupe do pelnego przysiadu!" + " Zrobiles dopiero " + przysiady/2 + " pelne przysiady!");
 		else
-			System.out.println("Nie len sie! Jestes na dole. Rusz dupe do pelnego przysiadu!" + " Zrobiles dopiero " + przysiady/2 + " przysiadow!");
+			System.out.println("Nie len sie! Jestes na dole. Rusz dupe do pelnego przysiadu!" + " Zrobiles dopiero " + przysiady/2 + " pelnych przysiadow!");
 	}
+	
 
 }
 }
